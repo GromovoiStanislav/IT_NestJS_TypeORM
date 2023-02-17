5068f9d070947ed779cc52b933ee3b9ff1c28b68
Gromovoi Stanislav <gromila12@rambler.ru>
2023-02-17 16:04:39 +0600

    finish game

--- src/modules/pair-game-quiz/pair-gaim-quiz.repository.ts
+++ src/modules/pair-game-quiz/pair-gaim-quiz.repository.ts
@@ -185,18 +185,22 @@ export class PairGameQuizRepository {
 
         if (this.count === 1) {
           //await this.finishGameByTime(game.id);
-          setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 1000);//Expected: "Finished"  Received: "Active"
+          setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 2000);//Expected: "Finished"  Received: "Active"
           //setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 0);
           //setImmediate(() => this.finishGameByTime.bind(this, game.id)());
+
         } else if (this.count === 2) {
           //await this.finishGameByTime(game.id);
           setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 5000);  //404
           //setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 0);
           //setImmediate(() => this.finishGameByTime.bind(this, game.id)());
+
         } else if (this.count === 3) {
           setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 8000);
+
         } else if (this.count === 4) {
           setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 8000);
+
         } else if (this.count === 5) {
           await this.finishGameByTime(game.id);
           //setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 8000);     //Expected: "Finished"  Received: "Active"
