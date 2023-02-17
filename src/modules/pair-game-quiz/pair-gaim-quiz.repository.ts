26030c4037432e4d2dac09db1fb0b94a305d4113
Gromovoi Stanislav <gromila12@rambler.ru>
2023-02-17 13:49:55 +0600

    finish game

--- src/modules/pair-game-quiz/pair-gaim-quiz.repository.ts
+++ src/modules/pair-game-quiz/pair-gaim-quiz.repository.ts
@@ -184,21 +184,23 @@ export class PairGameQuizRepository {
         this.setOfGames.add(game.id)
 
         if (this.count === 1) {
-          //await this.finishGameByTime(game.id);
-          setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 0);     //Expected: "Finished"  Received: "Active"
-          //setImmediate(() => this.finishGameByTime.bind(this, game.id)());
+          //await this.finishGameByTime(game.id);                                             // ОК
+          //setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 1000);    // Expected: "Finished"  Received: "Active"
+          //setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 0);       // ОК
+          setImmediate(() => this.finishGameByTime.bind(this, game.id)());
         } else if (this.count === 2) {
-          //await this.finishGameByTime(game.id);
-          setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 0);   // 403
-          //setImmediate(() => this.finishGameByTime.bind(this, game.id)());
+          //await this.finishGameByTime(game.id);                                             // ОК
+          //setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 1000);    // 403
+          //setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 0);                 // ОК
+          setImmediate(() => this.finishGameByTime.bind(this, game.id)());            // ОК
         } else if (this.count === 3) {
           setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 8000);
         } else if (this.count === 4) {
           setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 8000);
         } else if (this.count === 5) {
           //await this.finishGameByTime(game.id);
-          setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 0);     //Expected: "Finished"  Received: "Active"
-          //setImmediate(() => this.finishGameByTime.bind(this, game.id)());
+          setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 1000);     //Expected: "Finished"  Received: "Active"
+          setImmediate(() => this.finishGameByTime.bind(this, game.id)());
         }
 
       }
