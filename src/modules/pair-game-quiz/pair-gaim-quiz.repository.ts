8a845351ebe010e37313efa756a277431b74a6a2
Gromovoi Stanislav <gromila12@rambler.ru>
2023-02-17 14:58:24 +0600

    finish game

--- src/modules/pair-game-quiz/pair-gaim-quiz.repository.ts
+++ src/modules/pair-game-quiz/pair-gaim-quiz.repository.ts
@@ -189,8 +189,8 @@ export class PairGameQuizRepository {
           //setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 0);
           setImmediate(() => this.finishGameByTime.bind(this, game.id)());
         } else if (this.count === 2) {
-          await this.finishGameByTime(game.id);                                             // ОК
-          //setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 1000);    // 403
+          //await this.finishGameByTime(game.id);                                             // ОК
+          setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 8000);    // 403
           //setTimeout(() => this.finishGameByTime.bind(this, game.id)(), 0);
           //setImmediate(() => this.finishGameByTime.bind(this, game.id)());
         } else if (this.count === 3) {
